9d5f7c8baa8ee3bf9ca88ff32cdea62bac02fcf1
David Tolpin <david.tolpin@gmail.com>
2011-06-07 01:28:41 +0300

    treeexp: code cleanup

--- code/treeexp/treeexp.lisp
+++ code/treeexp/treeexp.lisp
@@ -2,48 +2,54 @@
   (:documentation "Comparing MCTS algorithms on search trees")
   (:use "COMMON-LISP" "MCTS" "COMMON-LISP-USER")
   (:export "EXPERIMENT"
+           "EXPERIMENTS" "VARARM-EXPERIMENTS"
+           "MANY-EXPERIMENTS"
            "MAKE-TREE"
-           "MAX-MEAN"
+           "BEST-MEAN"
+           "*MAKE-ARM*"
+           "*MAKE-ALPHA-SWITCH*"
+           "*MAKE-BETA-SWITCH*"
+           "*CHOOSE*"
            "+RANDOM-TREE-2X2+"
            "+FIXED-TREE-3X2+"))
 (in-package "TREEEXP")
 
 (defconstant +fringe-width+ 1)
 
-(defun make-fringe (make-arm)
+(defun make-fringe ()
   "make a random fringe with mean 0.5"
-  (map 'vector (lambda (m) (funcall make-arm :mean m))
+  (map 'vector (lambda (m) (funcall *make-arm* :mean m))
        (loop repeat +fringe-width+
           append (let ((v (random 1.0))) (list v (- 1.0 v))))))
-                    
-(defun make-tree (levels branching make-arm 
+
+(defvar *make-arm* #'make-armf)
+(defvar *make-alpha-switch* #'make-switch)
+(defvar *make-beta-switch* #'make-switch)
+(defvar *choose* #'max)
+
+(defun make-tree (levels branching
                   &optional
-                  (make-aswitch #'make-antiswitch) (make-bswitch #'make-antiswitch))
+                  (make-alpha-switch *make-alpha-switch*) (make-beta-switch *make-beta-switch*))
   "make a random tree"
-  (funcall make-aswitch 
+  (funcall make-alpha-switch 
    :nodes (if (= levels 1)
-              (make-fringe make-arm)
+              (make-fringe)
               (coerce (loop repeat branching
-                         collect (make-tree (1- levels) branching make-arm
-                                            make-bswitch make-aswitch))
+                         collect (make-tree (1- levels) branching
+                                            make-beta-switch make-alpha-switch))
                       'vector))))
 
-(defun max-mean (tree)
+(defun best-mean (tree)
   "find the max mean of arms"
   (etypecase tree
     (arm (arm-mean tree))
-    (switch (reduce #'min (map 'list #'max-mean (switch-nodes tree))))))
+    (switch (reduce *choose* (map 'list #'best-mean (switch-nodes tree))))))
 
-(defun experiment (levels branching make-arm make-bswitch sampling-factor nruns
-                   &key (vararm nil))
-  (flet ((avgrwd (select)
+(defun experiment (&key levels branching sampling-factor nruns vararm)
+   (flet ((avgrwd (select)
            (/ (float (loop repeat nruns
-                        sum (let* ((tree (with-unique-node-ids
-                                             (make-tree
-                                              levels branching
-                                              make-arm
-                                              #'make-switch make-bswitch))))
-                              (- (max-mean tree) 
+                        sum (let* ((tree (with-unique-node-ids (make-tree levels branching))))
+                              (- (best-mean tree)
                                  (pull-best-arm tree
                                                 select sampling-factor)))))
               nruns)))
@@ -55,19 +61,16 @@
               (if vararm branching sampling-factor) vcreg ucreg uvreg rnreg)))
   (force-output *standard-output*))
 
-(defun experiments (levels branching make-arm make-bswitch min-sf sf-step n-sf nruns
-                    &key (vararm nil))
+(defun experiments (&key levels branching min-sf sf-step n-sf nruns)
   (format t "nsamples r_vct r_uct r_uvt r_random~%")
   (loop for sf = min-sf then (round (* sf sf-step)) repeat n-sf
-       do (experiment levels branching make-arm make-bswitch sf nruns :vararm vararm)))
+       do (experiment :levels levels :branching branching :sampling-factor sf :nruns nruns)))
 
-(defun vararm-experiments (levels sampling-factor make-arm make-bswitch
-                           min-b b-step n-b nruns)
+(defun vararm-experiments (&key levels sampling-factor min-b b-step n-b nruns)
   (format t "nsamples r_vct r_uct r_uvt r_random~%")
   (loop for b = min-b then (round (* b b-step)) repeat n-b
-     do (experiment levels b make-arm make-bswitch sampling-factor
-                    (ceiling (/ nruns (log b 2)))
-                    :vararm t)))
+     do (experiment :levels levels :branching b :sampling-factor sampling-factor
+                    :nruns (ceiling (/ nruns (log b 2))) :vararm t)))
 
 (defconstant +number-of-runs+ 16000)
 
@@ -77,7 +80,8 @@
                                                 :direction :output
                                                 :if-exists :supersede
                                                 :if-does-not-exist :create)
-          (experiments 2 b #'make-armb #'make-switch 4 1.3 12 (ceiling (/ +number-of-runs+ (log b 2)))))))
+          (experiments :levels 2 :branching b :min-sf 4 :sf-step 1.3 :n-sf 12 
+                       :nruns (ceiling (/ +number-of-runs+ (log b 2)))))))
 
 ;; Predefined trees for testing 
 
