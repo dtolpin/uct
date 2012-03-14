8f13f274ea8cf8a258ff12f35175589cc031bc4a
David Tolpin <david.tolpin@gmail.com>
2012-03-14 11:47:46 +0200

    answers to the questions

--- code/treeexp/mcts.lisp
+++ code/treeexp/mcts.lisp
@@ -398,6 +398,22 @@
     "finds root of f in interval a b"
     (bs f a b (funcall f a) (funcall f b) eps)))
 
+;; computing erf, the error function
+(let ((a1 0.254829592d0)
+      (a2 -0.284496736d0)
+      (a3 1.421413741d0)
+      (a4 -1.453152027d0)
+      (a5 1.061405429d0)
+      (p 0.3275911d0))
+
+  (defun erf (x)    
+    "ERF, the error function, approximation according to A&S 7.1.26"
+    (let* ((y (/ 1.0d0 (+ 1.0d0 (* p (abs x)))))
+           (z (- 1.0d0
+                 (* y (exp (- (* x x))) 
+                    (+ a1 (* y (+ a2 (* y (+ a3 (* y (+ a4 (* y a5))))))))))))
+      (if (plusp x) z (- z)))))
+
 ;; Hoeffding with Eyal's correction
 (flet ((estimate (n over under)
          (flet ((destim (between)
